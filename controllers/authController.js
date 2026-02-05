a25d60c1c69a38b53c297cec9c1ebef7867bcbc7
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-02-05 05:07:08 +0000

    folder structure, import and export format, 2nd bracket to make it an OBJECT... CMCMRU = controllers, middlewares, confiq, models, routes and utils. folder structure for backend...

---
+++ controllers/authController.js
@@ -0,0 +1,45 @@
+
+const registrationController = (req, res) => {
+    // require.bode te sob data thakbe, destructure kora hoise
+    const { username, email, password } = req.body
+
+    // conditional validation with error message and status work
+    const errors = []
+    const successStatus = 201
+    if (!successStatus) {
+        errors.push({
+            errorType: 'successStatus',
+            message: 'Server Error...',
+        })
+    }
+
+
+    if (!username) {
+        errors.push({
+            errorType: 'username',
+            message: 'username required',
+        })
+    }
+    if (!email) {
+        errors.push({
+            errorType: 'email',
+            message: 'email required',
+        })
+    }
+    if (!password) {
+        errors.push({
+            errorType: 'password',
+            message: 'password required'
+        })
+    }
+
+    res.status(successStatus).send({
+        success: errors.length == 0 ? true : false,
+        errors: errors.length == 0 ? null : errors,
+        message: errors.length == 0 ? "Registration Successful" : "Registration Failed",
+    })
+
+}
+// {} 2nd bracket deye OBJECT akare pathano hoi... import object akare hobe...
+
+module.exports = {registrationController}
--- index.js
+++ index.js
@@ -1,50 +1,11 @@
 const express = require('express')
+const { registrationController } = require('./controllers/authController')
 const app = express()
 
 // string data k json or object convert korar middleware
 app.use(express.json())
 
-app.post("/registration", (req, res) => {
-    // require.bode te sob data thakbe, destructure kora hoise
-    const { username, email, password } = req.body
-    
-    // conditional validation with error message and status work
-    const errors = []
-    const successStatus = 201
-    if (!successStatus) {
-        errors.push({
-            errorType: 'successStatus',
-            message: 'Server Error...',
-        })
-    }
-    
-
-    if (!username) {
-        errors.push({
-            errorType : 'username',
-            message : 'username required',
-        })
-    }
-    if (!email) {
-        errors.push({
-            errorType: 'email',
-            message: 'email required',
-        })
-    }
-    if (!password) {
-        errors.push({
-            errorType: 'password',
-            message : 'password required'
-        })
-    }
-
-    res.status(successStatus).send({
-        success: errors.length == 0 ? true : false,
-        errors: errors.length == 0 ? null : errors,
-        message: errors.length == 0 ? "Registration Successful" : "Registration Failed",
-    })
-
-})
+app.post("/registration", registrationController )
 
 app.listen(8000, () => {
     console.log("Server running successfully");
